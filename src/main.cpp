c265a17dd817dfbf55b74db08e40f03510e7370f
truebucha <truebucha@gmail.com>
2018-04-01 14:02:24 +0300

    prepare code for event loop implementation

--- src/main.cpp
+++ src/main.cpp
@@ -5,96 +5,343 @@
  * then off for one second, repeatedly.
  */
 #include "Arduino.h"
+#include <ArduinoOTA.h>
+
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
+
 #include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266HTTPUpdateServer.h>
 
 #define ADS1115_SERIAL_DEBUG
 #include "ADS1115.h"
 
 
-#define led_green 13
-#define led_yellow 12
-#define led_red 14
+#define greenLedPin 13
+#define yellowLedPin 12
+#define redLedPin 14
 #define beeper 16
-#define led_chip_blue 2
+#define blueChipLedPin 2
 #define button 2
 
+#define logToSerial Serial
 
-// replace with your channel’s thingspeak API key,
-// tutorial https://learn.sparkfun.com/tutorials/esp8266-thing-hookup-guide/example-sketch-ap-web-server
+// =============================================
+// global vars
 
-const char WiFiApName[] = "sensor-co-";
+int analog = 0;
+float corrected = 0.0;
+float coPpm = 0.0;
+float temp = 0.0;
+float pressure = 0.0;
+float humidity = 0.0;
+
+unsigned long lastEventCheckTime = 0;
+int eventDelayCountdown = 0;
+
+// ============================================
+
+const char WiFiApName[] = "sensor-co";
 const char WiFiApSecret[] = "Sensor123";
-WiFiServer server(8090);
 
-Adafruit_BME280 bme; // I2C
+String logStorage = String();
+
+ESP8266WebServer httpServer(8090);
+ESP8266HTTPUpdateServer httpUpdater;
+
+Adafruit_BME280 bme; 
 ADS1115 ads0(0x48); 
-// Adafruit_ADS1115 ads; 
 
+// ============================================
+// logging 
+
+void cutLog() {
+
+  if (logStorage.length() > 2000) {
+
+    logStorage = logStorage.substring(100);
+  }
+}
+
+template <typename T>
+void LOG(T t) {
+  
+  logStorage += String(t);
+  logStorage += String("\n");
+  cutLog();
+
+  #ifdef logToSerial
+    logToSerial.println(t);
+  #endif
+}
+
+// recursive variadic function
+template<typename T, typename... Args>
+void LOG(T t, Args... args) {
+
+    logStorage += String(t);
+    cutLog();
+
+    LOG(args...);
+}
+
+// ====================================
+// event handling
+
+void applyEventDelay(int delay) {
+
+  eventDelayCountdown = delay;
+}
+
+bool couldProcessNextEvent() {
+  
+  if (eventDelayCountdown <= 1) {
+
+    eventDelayCountdown = 0;
+    return true;
+  }
+
+  unsigned long currentTime = millis();
+  unsigned long timeElapsed = currentTime - lastEventCheckTime;
+  lastEventCheckTime = currentTime;
+
+  // String uptimeMessage = String((F((F("event time elapsed: ")))));
+  // uptimeMessage += String(timeElapsed);
+  // uptimeMessage += String((F((F("ms")))));
+  // LOG(uptimeMessage);
+
+  eventDelayCountdown -= timeElapsed;
+  
+  return false;
+}
+
+// ===========================
+// State
+
+String stateString() {
+  
+  String result = String();
+
+  result += ((F("\ndevice config\n")));
+
+  result += ((F("\nSketch size: ")));
+  result += String(ESP.getSketchSize());
+
+  result += ((F("\nFree size: ")));
+  result += String(ESP.getFreeSketchSpace());
+
+  result += ((F("\nFree Heap: ")));
+  result += String(ESP.getFreeHeap());
+
+  return result;
+}
+
+// ============================
+
+float readADSValues() {
+
+  LOG((F("Sensor 1 ************************")));
+  // Set the gain (PGA) +/- 1.024v
+  // ads0.setGain(ADS1115_PGA_0P256);
+
+  // Get the number of counts of the accumulator
+  LOG((F("Counts for sensor 1 is:")));
+  
+  // The below method sets the mux and gets a reading.
+  int sensorOneCounts=ads0.getConversionP2N3();  // counts up to 16-bits  
+  LOG(sensorOneCounts);
+
+  // To turn the counts into a voltage, we can use
+  LOG((F("Voltage for sensor 1 is:")));
+  //  float value = sensorOneCounts * ads0.getMvPerCount();
+  float value = ads0.getMilliVolts();
+  //  float value = sensorOneCounts * 0.007813;
+  
+  LOG(String(value, 8));   
+   
+  // // 2nd sensor is on P2/N3 (pins 6/7)
+  // LOG((F("Sensor 2 ************************")));
+  // // Set the gain (PGA) +/- 0.256v
+  // adc0.setGain(ADS1115_PGA_0P256);
+
+  // // Manually set the MUX  // could have used the getConversionP* above
+  // adc0.setMultiplexer(ADS1115_MUX_P2_N3); 
+  // LOG((F("Counts for sensor 2 is:")));
+  // LOG(adc0.getConversion());  
+
+  // LOG((F("mVoltage sensor 2 is:")));
+  // LOG(adc0.getMilliVolts());  // Convenience method to calculate voltage
+
+  // LOG();
+  
+  return value;
+}
+
+// =============================
+// responds
+
+void respondWithState() {
+
+  String response = stateString();
+
+  httpServer.send(200, (F("text/plain")), response.c_str() );
+}
+
+void respondWithLog() {
+
+  String response = String(F("<html><body><p>"));
+  response += String(F("<br/>in Access Point mode: "));
+  response += String(WiFi.getMode() == WIFI_AP ? (F("YES")):(F("NO")));
+  // response += String(F("<br/>apnStartTime: "));
+  // response += String(apnStartTime);
+
+  // response += String(F("<br/>Guard Pin Raised: "));
+  // response += String(digitalRead(guardPin) == 1 ? (F("YES")):(F("NO")));
+  // response += String(F("<br/>Alarm Pin Rased: "));
+  // response += String(digitalRead(alarmPin) == 1 ? (F("YES")):(F("NO")));
+  // response += String(F("<br/>Next Event Countdown: "));
+  // response += String(eventDelayCountdown);
+  // response += String(F("<br/>Modem Network Connection Sequental Failures Count: "));
+  // response += String(modemConnectionSequentalFailuresCount);
+  response += String((F("</p><pre>")));
+  response += logStorage;
+  response += String((F("</pre></body></html>\n\r")));
+  
+  httpServer.send(200, (F("text/html")), response.c_str());
+}
+
+void respondWithMeasured() {
+  // Check if a client has connected
+
+  digitalWrite(blueChipLedPin, HIGH);
+
+  // if (req.indexOf((F("/led/0"))) != -1)
+
+  // Prepare the response. Start with the common header:
+  // readCoValues();
+  // readBmeValues();
+
+  float a = readADSValues();
+
+  // String s = "HTTP/1.1 200 OK\r\n";
+  // s += "Content-Type: application/json\r\n\r\n";
+  String response = String((F("{")));
+
+  response += String((F("\"analog\":")));
+  response += String(a, 8);
+  response += String((F(",")));
+
+  response += String((F("\"coPpm\":")));
+  response += String(coPpm, 8);
+  response += String((F(",")));
+
+  response += String((F("\"temp\":")));
+  response += String(temp, 8);
+  response += String((F(",")));
+
+  response += String((F("\"pressure\":")));
+  response += String(pressure, 8);
+  response += String((F(",")));
+
+  response += String((F("\"humidity\":")));
+  response += String(humidity, 8);
+
+  response += String((F("}")));
+
+  // Send the response to the client
+  httpServer.send(200, (F("application/json")), response.c_str());
+  LOG((F("Client disonnected")));
+  digitalWrite(blueChipLedPin, LOW);
+  // The client will actually be disconnected
+  // when the function returns and 'client' object is detroyed
+}
+
+// =============================
+// WiFi
+
+String wifiApName() {
+
+  byte mac[6];
+   WiFi.macAddress(mac);
+
+   LOG((F("MAC: ")));
+   LOG(mac[5],HEX);
+   LOG((F(":")));
+   LOG(mac[4],HEX);
+   LOG((F(":")));
+   LOG(mac[3],HEX);
+   LOG((F(":")));
+   LOG(mac[2],HEX);
+   LOG((F(":")));
+   LOG(mac[1],HEX);
+   LOG((F(":")));
+   LOG(mac[0],HEX);
+
+   String result = String(WiFiApName);
+   result += String((F(" - ")));
+   result += String(mac[5], HEX);
+   result += String(mac[4], HEX);
+
+   return result;
+}
 
 void printButton() {
 
-  Serial.print((F("button pressed")));
+  LOG(((F("button pressed"))));
 }
+
 /**************************
  *   S E T U P
  **************************/
 
  void setupWiFi() {
-   byte mac[6];
-   WiFi.macAddress(mac);
-
-   Serial.print(F("MAC: "));
-   Serial.print(mac[5],HEX);
-   Serial.print(F(":"));
-   Serial.print(mac[4],HEX);
-   Serial.print(F(":"));
-   Serial.print(mac[3],HEX);
-   Serial.print(F(":"));
-   Serial.print(mac[2],HEX);
-   Serial.print(F(":"));
-   Serial.print(mac[1],HEX);
-   Serial.print(F(":"));
-   Serial.println(mac[0],HEX);
-
-   String apName = WiFiApName;
-   apName += String(mac[5], HEX);
-   apName += String(mac[4], HEX);
-
-   Serial.print(F("ApName: "));
-   Serial.println(apName);
-
-   WiFi.mode(WIFI_AP);
-
-   char ApName[15];
-   apName.toCharArray(ApName, 15);
-
-   WiFi.softAP(ApName, WiFiApSecret);
-   Serial.println(F("Finished with wifi setup"));
+  
+   WiFi.softAP(wifiApName().c_str(), WiFiApSecret);
+   LOG((F("Finished with wifi setup")));
  }
 
- void initBme() {
+ void setupServer() {
+
+  MDNS.begin("espco");
+
+  httpUpdater.setup(&httpServer, "/update");
+
+  // httpServer.on("/",[](){httpServer.send(200,"text/plain","Hello World!");});
+
+  httpServer.on("/", respondWithState);
+  httpServer.on("/log", respondWithLog);
+  httpServer.on("/measure", respondWithMeasured);
+
+  httpServer.begin();
+
+  MDNS.addService("http", "tcp", 80);
+ }
+
+ void setupBme() {
+
    if (!bme.begin(0x76)) {
-     Serial.println("Could not find a valid bme280 sensor, check wiring!");
+     LOG((F("Could not find a valid bme280 sensor, check wiring!")));
      //ESP.deepSleep( 30 * 1000000, WAKE_RF_DISABLED);
    } else {
-     Serial.println(F("BME280 found!"));
+     LOG((F("BME280 found!")));
    }
-   Serial.println("Finished with BME setup");
+   LOG((F("Finished with BME setup")));
  }
 
- void initADS() {
-  Serial.println(F("start ads"));
+ void setupADS() {
+
+  LOG((F("start ads")));
   ads0.initialize();
-  Serial.println("Getting single-ended readings from AIN0..3");
+  LOG((F("Getting single-ended readings from AIN0..3")));
   
   
   if (!ads0.testConnection()) {
-    Serial.println(F("Could not find a valid ADS1115, check wiring!"));
+    LOG((F("Could not find a valid ADS1115, check wiring!")));
   } else {
-    Serial.println(F("ADS1115 found!"));
+    LOG((F("ADS1115 found!")));
   }
 
   // adc0.setMode(ADS1115_MODE_CONTINUOUS);
@@ -105,50 +352,62 @@ void printButton() {
 
   #ifdef ADS1115_SERIAL_DEBUG
   ads0.showConfigRegister();
-  Serial.print("HighThreshold="); Serial.println(ads0.getHighThreshold(),BIN);
-  Serial.print("LowThreshold="); Serial.println(ads0.getLowThreshold(),BIN);
+  LOG((F("HighThreshold="))); LOG(ads0.getHighThreshold(),BIN);
+  LOG((F("LowThreshold="))); LOG(ads0.getLowThreshold(),BIN);
   #endif
 
   // pinMode(1,INPUT_PULLUP);
   // ads0.setConversionReadyPinMode();
 
-  Serial.println(F("Finished with ADS1115 setup"));
+  LOG((F("Finished with ADS1115 setup")));
 }
 
-void initHardware() {
+void setupSOC() {
 
-  pinMode(led_chip_blue, OUTPUT);
+  pinMode(blueChipLedPin, OUTPUT);
   pinMode(beeper, OUTPUT);
-  pinMode(led_green, OUTPUT);
-  pinMode(led_yellow, OUTPUT);
-  pinMode(led_red, OUTPUT);
+  pinMode(greenLedPin, OUTPUT);
+  pinMode(yellowLedPin, OUTPUT);
+  pinMode(redLedPin, OUTPUT);
   pinMode(button, INPUT);
 
-  attachInterrupt(button, printButton, FALLING);
-   Wire.begin(5,4);
+  Wire.begin(5,4);
 
-  //  initBme();
-   initADS();
-   Serial.println("Finished with hardware setup");
-   // Don't need to set ANALOG_PIN as input,
-   // that's all it can be.
+  attachInterrupt(button, printButton, FALLING);
+
+  LOG((F("SOC setup finished")));
+}
+
+void prepareTestPinsState() {
+
+  digitalWrite(beeper, LOW);
+  digitalWrite(redLedPin, HIGH);
+  digitalWrite(yellowLedPin, HIGH);
+  digitalWrite(greenLedPin, HIGH);
+  digitalWrite(blueChipLedPin, LOW);
 }
 
 void setup() {
-  Serial.begin(9600);
-  delay(10);
-  Serial.println("Start board setup");
-  initHardware();
-  digitalWrite(led_chip_blue, HIGH);
-  setupWiFi();
-  server.begin();
-  digitalWrite(led_chip_blue, LOW);
-  Serial.println("Finished with setup");
 
-  digitalWrite(beeper, LOW);
-  digitalWrite(led_red, HIGH);
-  digitalWrite(led_yellow, HIGH);
-  digitalWrite(led_green, HIGH);
+  
+
+  logToSerial.begin(74880);
+  delay(10);
+
+  LOG((F("Starting setup of the device")));
+
+  setupSOC();
+
+  //  setupBme();
+  setupADS();
+
+  setupServer();
+
+  setupWiFi();
+
+  //prepareTestPinsState();
+  
+  LOG((F("Finished setup of the device")));
 }
 
   /**************************
@@ -162,9 +421,9 @@ int correctedAnalogRead(int value) {
   // measured 3.12 v source stabilazed voltage
   //          Usource(mV) / stepsTotal * currentSteps
   // float result = (3120.0 / float(1024.0-zeroVoltageValue) * float(input));
-  // Serial.print("read :");
-  // Serial.println(value);
-  // Serial.println(result);
+  // LOG((F("read :")));
+  // LOG(value);
+  // LOG(result);
   return input;
 }
 
@@ -173,19 +432,14 @@ float ppmUsingMiliVolts(float mV) {
   float nA = mV * (1000000 / 50000);
   //                  sensor current  * sensor nA->ppm coefficient / amplifier coefficient * correction;
   float result = nA * 1.4 / 108 * 6.11;
-  // Serial.print("ppm :");
-  // Serial.println(nA);
-  // Serial.println(result);
+  // LOG((F("ppm :")));
+  // LOG(nA);
+  // LOG(result);
   return result;
 }
 
 
-int analog = 0;
-float corrected = 0.0;
-float coPpm = 0.0;
-float temp = 0.0;
-float pressure = 0.0;
-float humidity = 0.0;
+
 
 void readBmeValues() {
   temp = bme.readTemperature();
@@ -193,47 +447,7 @@ void readBmeValues() {
   humidity = bme.readHumidity();
 }
 
-float readADSValues() {
-  Serial.println("Sensor 1 ************************");
-  // Set the gain (PGA) +/- 1.024v
-  // ads0.setGain(ADS1115_PGA_0P256);
-
-  // Get the number of counts of the accumulator
-  Serial.print("Counts for sensor 1 is:");
-  
-  // The below method sets the mux and gets a reading.
-  int sensorOneCounts=ads0.getConversionP2N3();  // counts up to 16-bits  
-  Serial.println(sensorOneCounts);
-
-  // To turn the counts into a voltage, we can use
-  Serial.print("Voltage for sensor 1 is:");
-  //  float value = sensorOneCounts * ads0.getMvPerCount();
-  float value = ads0.getMilliVolts();
-  //  float value = sensorOneCounts * 0.007813;
-  Serial.println(value, 8);
-  
-  Serial.println();
-   
-   
-  // // 2nd sensor is on P2/N3 (pins 6/7)
-  // Serial.println("Sensor 2 ************************");
-  // // Set the gain (PGA) +/- 0.256v
-  // adc0.setGain(ADS1115_PGA_0P256);
-
-  // // Manually set the MUX  // could have used the getConversionP* above
-  // adc0.setMultiplexer(ADS1115_MUX_P2_N3); 
-  // Serial.print("Counts for sensor 2 is:");
-  // Serial.println(adc0.getConversion());  
-
-  // Serial.print("mVoltage sensor 2 is:");
-  // Serial.println(adc0.getMilliVolts());  // Convenience method to calculate voltage
-
-  // Serial.println();
-  
-  return value;
-}
-
-void readChipAnalog() {
+void readSocAnalog() {
   analog = analogRead(0);
   corrected = correctedAnalogRead(analog);
   coPpm = corrected * 3.5;
@@ -242,84 +456,30 @@ void readChipAnalog() {
 void loopMeasure() {
   delay(1e3);
 
-  digitalWrite(led_chip_blue, HIGH);
+  digitalWrite(blueChipLedPin, HIGH);
   float a = readADSValues();
-  Serial.print(F("analog = "));
-  Serial.print(a, 8);
-  Serial.println(F(" mV"));
-  digitalWrite(led_chip_blue, LOW);
+  LOG((F("analog = ")));
+  LOG(a, 8);
+  LOG((F(" mV")));
+  digitalWrite(blueChipLedPin, LOW);
 }
 
 void loop_check() {
   delay(1e3);
-  readChipAnalog();
-  Serial.print(F("analog = "));
-  Serial.println(analog);
-  Serial.print(F("corrected = "));
-  Serial.println(corrected);
-  Serial.println(F("CO = "));
-  Serial.println(coPpm);
-  Serial.println(F("=="));
-}
-
-void loop_work() {
-  // Check if a client has connected
-  WiFiClient client = server.available();
-  if (!client) {
-    return;
-  }
-
-  digitalWrite(led_chip_blue, HIGH);
-
-  // Read the first line of the request
-  String req = client.readStringUntil('\r');
-  Serial.println(req);
-
-  client.flush();
-
-  // if (req.indexOf("/led/0") != -1)
-
-  // Prepare the response. Start with the common header:
-  // readCoValues();
-  // readBmeValues();
-  float a = readADSValues();
-
-  String s = "HTTP/1.1 200 OK\r\n";
-  s += "Content-Type: application/json\r\n\r\n";
-  s += "{";
-
-  s += "\"analog\":";
-  s += String(a, 8);
-  s += ",";
-
-  s += "\"coPpm\":";
-  s += String(coPpm, 8);
-  s += ",";
-
-  s += "\"temp\":";
-  s += String(temp, 8);
-  s += ",";
-
-  s += "\"pressure\":";
-  s += String(pressure, 8);
-  s += ",";
-
-  s += "\"humidity\":";
-  s += String(humidity, 8);
-
-  s += "}";
-
-  // Send the response to the client
-  client.print(s);
-  delay(1);
-  Serial.println("Client disonnected");
-  digitalWrite(led_chip_blue, LOW);
-  // The client will actually be disconnected
-  // when the function returns and 'client' object is detroyed
+  readSocAnalog();
+  LOG((F("analog = ")));
+  LOG(analog);
+  LOG((F("corrected = ")));
+  LOG(corrected);
+  LOG((F("CO = ")));
+  LOG(coPpm);
+  LOG((F("==")));
 }
 
 void loop() {
   //loop_check();
-  loop_work();
   // loopMeasure();
+
+  httpServer.handleClient();
+  yield();
 }
